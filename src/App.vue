40cb69780ff659cd0f8597ef3063f003b7a74f7e
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-24 20:44:05 +0800

    4 - Single File Components
    4 - Single File Components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ name }}
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      name: "Ryan",
+    };
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
