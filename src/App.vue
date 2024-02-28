6e27e78f4357287c472d4dca139cd3456eb9fe55
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-28 20:48:51 +0800

    11 - Conditional Rendering (Part 1) - v-if, v-else-if, v-else and template tag
    11 - Conditional Rendering (Part 1) - v-if, v-else-if, v-else and template tag

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underline Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional movie</h2>
-  <h2 v:class="{
-    promoted: isPromoted,
-    new: !isSoldOut,
-    'sold-out': isSoldOut
-  }">Object conditional movie</h2>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not a number</h2>
 
-  <h2 :style="{
-    color: highlightcolor,
-    fontSize: headersize + 'px',
-    padding: '20px'
-  }">Inline Style</h2>
-
-  <h2 v-bind:style="headerStyleObject">Style Object</h2>
-
-  <div v-bind:style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div v-bind:style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+  <template v-if="display">
+    <h2>Vishwas</h2>
+    <h2>Codevolution</h2>
+    <h2>Vue</h2>
+  </template>
 
 </template>
 
@@ -33,34 +19,8 @@ export default {
   name: 'App',
   data() {
     return {
-      headingId: 'heading',
-      isDisabled: false,
-      status: 'success',
-      isPromoted: false,
-      isSoldOut: true,
-      highlightcolor: 'blue',
-      headersize: 50,
-      headerStyleObject: {
-        color: 'red',
-        fontSize: '50px',
-        padding: '20px'
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padding: '10px',
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green',
-        padding: '20px'
-      },
-      dangerStyleObject: {
-        color: 'darkred',
-        backgroundColor: 'red',
-        border: '1px solid darkred',
-      }
-
+      num: 1,
+      display: true
     };
   },
   components: {
@@ -78,20 +38,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted{
-  font-style: italic;
-}
-.new{
-  color: olivedrab;
-}
-.sold-out{
-  color: red;
-}
-
-
 </style>
