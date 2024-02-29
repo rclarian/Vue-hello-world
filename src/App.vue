f4fca792c71edadd4cbf5b7c46232b1d011f3f3b
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-29 21:21:05 +0800

    14 - Lists and Keys - v-for, important of keys
    14 - Lists and Keys - v-for, important of keys

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,27 @@
 <template>
-  
-  <h2 v-for="(name, index) in names" :key="name"> {{ index }} {{ name }}</h2>
-  <h2 v-for="name in fullNames" :key="name.first"> {{ name.first }} {{ name.last }}</h2>
-
-  <div v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name }}</h2>
-    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-  </div>
-
-  <h2 v-for="(value, key, index) in myInfo" :key="index">{{ index }} {{ key }} {{ value }} </h2>
-
   <template v-for="name in names" :key="name">
     <h2>{{ name }}</h2>
+    <input type="text" placeholder="Last name">
     <hr />
   </template>
+  <button @click="shuffle">Shuffle!</button>
 
 </template>
 
 <script>
 
-
+import _ from "lodash";
 export default {
   name: 'App',
   data() {
     return {
-      names: ['Bruce', 'Clark', 'Diana', 'Ryan', 'Kulit'],
-      fullNames: [
-        { first: 'Bruce', last: 'Wayne'},
-        { first: 'Clark', last: 'Kent'},
-        { first: 'Princess', last: 'Diana'},
-      ],
-      actors: [
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'The Prestige'],
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titanic', 'Inception'],
-        },
-      ],
-      myInfo: {
-        name: 'Vishwas',
-        channel: 'Codevolution',
-        course: 'Vue 3',
-      }
+      names: ['Bruce', 'Clark', 'Joanne', 'Ryan', 'Kulit'],
+    }
+  },
+  methods: {
+    shuffle() {
+      console.log(this.names);
+      this.names = _.shuffle(this.names);
     }
   },
   components: {
