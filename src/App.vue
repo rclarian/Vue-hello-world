d79f68f31bf1506670f6a8dfd985ffb19a3a1983
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-29 21:00:03 +0800

    13 - List Rendering - v-for directives, array, array in array
    13 - List Rendering - v-for directives, array, array in array

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <h2 v-if="num === 0">The number is zero</h2>
-  <h2 v-else-if="num < 0">The number is negative</h2>
-  <h2 v-else-if="num > 0">The number is positive</h2>
-  <h2 v-else>Not a number</h2>
+  
+  <h2 v-for="(name, index) in names" :key="name"> {{ index }} {{ name }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first"> {{ name.first }} {{ name.last }}</h2>
 
-  <template v-if="display">
-    <h2>Vishwas</h2>
-    <h2>Codevolution</h2>
-    <h2>Vue</h2>
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{ actor.name }}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+  </div>
+
+  <h2 v-for="(value, key, index) in myInfo" :key="index">{{ index }} {{ key }} {{ value }} </h2>
+
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }}</h2>
+    <hr />
   </template>
-
-  <h2 v-show="showElement">Using v-show</h2>
-  <h2 v-if="showElement">Using v-if</h2>
 
 </template>
 
@@ -22,10 +24,28 @@ export default {
   name: 'App',
   data() {
     return {
-      num: 1,
-      display: true,
-      showElement: false
-    };
+      names: ['Bruce', 'Clark', 'Diana', 'Ryan', 'Kulit'],
+      fullNames: [
+        { first: 'Bruce', last: 'Wayne'},
+        { first: 'Clark', last: 'Kent'},
+        { first: 'Princess', last: 'Diana'},
+      ],
+      actors: [
+        {
+          name: 'Christian Bale',
+          movies: ['Batman', 'The Prestige'],
+        },
+        {
+          name: 'Di Caprio',
+          movies: ['Titanic', 'Inception'],
+        },
+      ],
+      myInfo: {
+        name: 'Vishwas',
+        channel: 'Codevolution',
+        course: 'Vue 3',
+      }
+    }
   },
   components: {
     
