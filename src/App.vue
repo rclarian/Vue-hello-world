bc65326d911a5a9bccc84f39fb9db8daadd18df6
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-02 19:28:21 +0800

    26 - Computed Setter
    26 - Computed Setter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
   <h2>Computed fullname - {{ fullName }}</h2>
+
+  <button @click="changeFullName">Change fullname</button>
+
   <h2>Inline Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}</h2>
   <button @click="items.push({id: 4, title: 'Keyboard', price: 50})">Add Item</button>
   <h2>Computed Total - {{ total }}</h2>
@@ -48,13 +51,23 @@ export default {
     getTotal(){
       console.log('getTotal Method');
       return this.items.reduce((total, curr) => (total = total + curr.price), 0);
+    },
+    changeFullName(){
+      this.fullName = 'Clark Kent';
     }
   },
   components: {
   },
   computed: {
-    fullName() {
-      return `${this.firstName} ${this.lastName}`;
+    fullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`
+      },
+      set(value){
+        const names = value.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+      }
     },
     total() {
       console.log('Total computed property');
