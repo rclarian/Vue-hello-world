4493b02b8dc763a55f9adf46dc2dd2f32ec3e587
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-04 21:20:44 +0800

    28 - Immediate and Deep Watchers
    28 - Immediate and Deep Watchers

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
   <button @click="volume += 2">Increase</button>
   <button @click="volume -= 2">Decrease</button>
 </div>
+<input type="text" v-model="movie">
+<input type="text" v-model="movieInfo.title" >
+<input type="text" v-model="movieInfo.actor" >
+<div>
+  <button @click="movieList = movieList.concat(['Wonder Woman'])">Add movie</button>
+</div>
 
 </template>
 
@@ -15,7 +21,13 @@ export default {
   name: 'App',
   data() {
     return {
-      volume: 0
+      volume: 0,
+      movie: 'batman',
+      movieInfo: {
+        title: '',
+        actor: '',
+      },
+      movieList: ['Batman', 'Superman']
     }
   },
   methods: {},
@@ -26,6 +38,24 @@ export default {
       if(newValue > oldValue && newValue === 16){
         alert('Listening to a high volume for a long time may change your hearing');
       }
+    },
+    movie: {
+      handler(newValue){
+        console.log(`Calling API with movie name = ${newValue}`);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue){
+        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`);
+      },
+      deep: true,
+    },
+    movieList: {
+      handler(newValue){
+        console.log(`Updated list ${newValue}`);
+      },
+      //deep: true,
     }
   },
 }
