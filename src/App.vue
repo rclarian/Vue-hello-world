d5e6423ef8302bbb8be115bb12108d04e05f00b4
Ryan Clarian <rclarian@gmail.com>
2024-03-05 09:56:45 +0800

    29 - Components - type vbase-css from Vue VSCode Snippets by sara drasber
    29 - Components - type vbase-css from Vue VSCode Snippets by sara drasber

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,26 @@
 <template>
-<h2>Volume Tracker (0-20)</h2>  
-<h3>Current Volume - {{ volume }}</h3>
-<div>
-  <button @click="volume += 2">Increase</button>
-  <button @click="volume -= 2">Decrease</button>
-</div>
-<input type="text" v-model="movie">
-<input type="text" v-model="movieInfo.title" >
-<input type="text" v-model="movieInfo.actor" >
-<div>
-  <button @click="movieList = movieList.concat(['Wonder Woman'])">Add movie</button>
-</div>
-
+  <div>
+    <GreetComponent />
+  </div>
 </template>
 
 <script>
+import GreetComponent from './components/GreetComponent.vue'
 
-//import _ from "lodash";
 export default {
   name: 'App',
+  components: {
+    GreetComponent,
+  },
   data() {
     return {
-      volume: 0,
-      movie: 'batman',
-      movieInfo: {
-        title: '',
-        actor: '',
-      },
-      movieList: ['Batman', 'Superman']
+      
     }
   },
   methods: {},
-  components: {},
   computed: {},
-  watch: {
-    volume(newValue, oldValue){
-      if(newValue > oldValue && newValue === 16){
-        alert('Listening to a high volume for a long time may change your hearing');
-      }
-    },
-    movie: {
-      handler(newValue){
-        console.log(`Calling API with movie name = ${newValue}`);
-      },
-      immediate: true,
-    },
-    movieInfo: {
-      handler(newValue){
-        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`);
-      },
-      deep: true,
-    },
-    movieList: {
-      handler(newValue){
-        console.log(`Updated list ${newValue}`);
-      },
-      //deep: true,
-    }
-  },
+  watch: {},
+  
 }
 </script>
 
---
+++ src/components/GreetComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>hello from GreetComponent</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GreetComponent',
+    }
+</script>
+
+<style scoped>
+
+</style>
