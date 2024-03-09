ec5ea96a44f4d6356615dabbfceaf310f9c8e76f
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-09 15:27:35 +0800

    35 - Validating Emitted Events
    35 - Validating Emitted Events

--- src/components/PopupComponent.vue
+++ src/components/PopupComponent.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h2>This is a Popup</h2>
-        <button @click="$emit('close', 'Vishwas')">Close Popup</button>
+        <input type="text" v-model="name">
+        <button @click="$emit('close', name)">Close Popup</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'PopupComponent',
-        emits: ['close']
+        emits: {
+            close: (name) => {
+                if(!name){
+                    return false
+                }else{
+                    return true
+                }
+            }
+        },
+        data() {
+            return {
+                name: '',
+            }
+        }
     }
 </script>
 
