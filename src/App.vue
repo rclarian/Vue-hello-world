6a8999ef3624e61f5747515cfca7f363788d939f
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-16 18:40:46 +0800

    43 - Teleport Component - pop up, teleport, https://codesandbox.io/
    43 - Teleport Component - pop up, teleport, https://codesandbox.io/

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <teleport to="#portal-root">
-    <PortalComponent />
-  </teleport>
+  <div class="container">
+    <div class="content">
+      <h1>My App</h1>
+      <p>
+        This is an example of how you might use Vue Teleport. I think it is a
+        pretty neat API that is yet another awesome escape hatch that Vue
+        provides for practical reasons. Sometimes you just need to render
+        something completely outside the Vue component tree.
+      </p>
+      <button @click="showModal = true">Show Modal</button>
+      <teleport to="#modal-root">
+        <ModalComponent v-show="showModal" @close="showModal = false">
+          This is the secret modal message!
+        </ModalComponent>
+      </teleport>
+    </div>
+  </div>
  
-
 </template>
 
 <script>
-import PortalComponent from './components/PortalComponent.vue'
+import ModalComponent from './components/ModalComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    PortalComponent,
+    ModalComponent,
   },
   data() {
     return {
-      activeTab: 'TabA'
+      showModal: false,
     }
   },
   provide() {
@@ -32,12 +45,39 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  height: 100%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  max-width: 400px;
+  position: relative;
+}
+
 </style>
