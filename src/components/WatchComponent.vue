e0ff0a40bc9ce5e9db23f1fc5bd658d4795548df
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-23 14:01:08 +0800

    59 - Replacing Watchers (Part 2) - ref, watch, reactive, cloneDeep toRefs,
    59 - Replacing Watchers (Part 2) - ref, watch, reactive, cloneDeep toRefs,

--- src/components/WatchComponent.vue
+++ src/components/WatchComponent.vue
@@ -3,17 +3,57 @@
         <!-- Options API -->
         <input type="text" placeholder="Name" v-model="name">
         <br>
+
         <!-- Composition API -->
         <input type="text" placeholder="First Name" v-model="firstName">
         <input type="text" placeholder="Last Name" v-model="lastName">
+
+        <!-- Reactive API -->
+        <input type="text" placeholder="Reactive First Name" v-model="fName">
+        <input type="text" placeholder="Reactive Last Name" v-model="lName">
+        <input type="text" placeholder="Reactive Hero Name" v-model="options.heroName">
     </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue'
+import { ref, watch, reactive, toRefs } from 'vue'
+import _ from 'lodash'
     export default {
         name: 'WatchComponent',
         setup() {
+            const state = reactive({
+                fName: '',
+                lName: '',
+                options: {
+                    heroName: ''
+                }
+            })
+
+            // watch(() => {
+            //     return { ...state }
+            // },
+            //     function (newValue, oldValue) {
+            //         console.log('fName Old value ', oldValue.fName)
+            //         console.log('fName New value ', newValue.fName)
+            //         console.log('lName Old value ', oldValue.lName)
+            //         console.log('lName New value ', newValue.lName)
+            //     }
+            // )
+            
+            watch(() => _.cloneDeep(state.options),
+            function(newValue, oldValue){
+                console.log('fName Old value ', oldValue)
+                console.log('fName New value ', newValue)
+            },{
+                deep: true
+            })
+
+            watch(() => state.fName,
+            function(newValue, oldValue){
+                console.log('fName Old value ', oldValue)
+                console.log('fName New value ', newValue)
+            })
+
             const firstName = ref('')
             const lastName = ref('Wayne')
             
@@ -28,7 +68,8 @@ import { ref, watch } from 'vue'
 
             return {
                 firstName,
-                lastName
+                lastName,
+                ...toRefs(state)
             }
         },
         data() {
