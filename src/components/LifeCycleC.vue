973dd5e4c7b243f32f0c300f604955be69f1cae1
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-24 19:59:03 +0800

    61 - Replacing Lifecycle Hooks with composition API
    61 - Replacing Lifecycle Hooks with composition API

---
+++ src/components/LifeCycleC.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        Composition API Lifecyle Hooks
+    </div>
+</template>
+
+<script>
+import { 
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
+ } from 'vue';
+    export default {
+        name: 'LifeCycleC',
+        setup() {
+            onBeforeMount(() => {
+                console.log('LifecycleC onBeforeMount')
+            })
+            onMounted(() => {
+                console.log('LifecycleC onMounted')
+            })
+            onBeforeUpdate(() => {
+                console.log('LifecycleC onBeforeUpdate')
+            })
+            onUpdated(() => {
+                console.log('LifecycleC onUpdated')
+            })
+            onBeforeUnmount(() => {
+                console.log('LifecycleC onBeforeUnmount')
+            })
+            onUnmounted(() => {
+                console.log('LifecycleC onUnmounted')
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
