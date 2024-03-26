a2d02511040b04fd8b0649ac83e2681610e32881
Ryan Clarian <rclarian@gmail.com>
2024-03-26 14:56:14 +0800

    64 - Custom Events and Composition API - child to parents
    64 - Custom Events and Composition API - child to parents

--- src/components/PersonC.vue
+++ src/components/PersonC.vue
@@ -3,7 +3,7 @@
         <input type="text" placeholder="First Name" v-model="firstName">
         <input type="text" placeholder="Last Name" v-model="lastName">
     </div>
-    <PersonGreeting :firstName="firstName" :lastName="lastName"/>
+    <PersonGreeting :firstName="firstName" :lastName="lastName" @callHeroes="callHeroes"/>
 </template>
 
 <script>
@@ -18,9 +18,14 @@ import PersonGreeting from './PersonGreeting.vue';
             const firstName = ref('')
             const lastName = ref('')
 
+            function callHeroes(hero){
+                alert(`Calling ${hero}`)
+            }
+
             return {
                 firstName,
                 lastName,
+                callHeroes
             }
         }
     }
--- src/components/PersonGreeting.vue
+++ src/components/PersonGreeting.vue
@@ -1,21 +1,32 @@
 <template>
     <div>Hello {{ firstName }} {{ lastName }}</div>
     <div>Hello {{ fullName }}</div>
+    <button @click="sendEvent">Call Heroes</button>
+
 </template>
 
 <script>
 import { computed } from 'vue'
     export default {
         name: 'PersonGreeting',
-        setup(props) {
+        setup(props, context) {
+            console.log(context)
             const fullName = computed(() => {
                 return `${props.firstName} ${props.lastName}`
             })
+
+            function sendEvent() {
+                context.emit('callHeroes', fullName.value)
+            }
+
             return {
                 fullName,
+                sendEvent
             }
         },
         props: ['firstName', 'lastName'],
+        emits: ['callHeroes'],
+
         // computed: {
         //     fullName() {
         //         return `${this.firstName} ${this.lastName}`
